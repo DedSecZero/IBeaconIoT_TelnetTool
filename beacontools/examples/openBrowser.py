ac281701599f2df99353ae3984ca57cea2c6e44f
Jgarciac3 <jgarciac3@ucentral.edu.co>
2018-04-19 00:37:34 +0000

    Update

--- beacontools/examples/openBrowser.py
+++ beacontools/examples/openBrowser.py
@@ -1,5 +1,6 @@
 import os,time
 print("Abriendo Navegador")
-os.system("sudo chromium-browser -no-sandbox http://www.google.com.co")
+#os.system("sudo chromium-browser -no-sandbox http://www.google.com.co")
+os.system("sudo chromium-browser -no-sandbox  http://172.71.10.23:8080/?action=stream")
 print("Delay fin python navegador")
 time.sleep(13)
--- beacontools/examples/scanner_ibeacon_example3.py
+++ beacontools/examples/scanner_ibeacon_example3.py
@@ -8,9 +8,7 @@ import threading, commands
 
 rangeMax = -40
 def openBrowserCam():
-   #os.system("sudo chromium-browser -no-sandbox http://172.71.10.23:8080/?action=stream")
-    #os.system("sudo chromium-browser -no-sandbox http://www.google.com.co")
-    os.system("sudo python openBrowser.py")
+   os.system("sudo python openBrowser.py")
     
 def openBrowserBlack():
     os.system("sudo chromium-browser -no-sandbox file:///home/pi/Desktop/prueba2/beacontools/examples/index.html")
@@ -23,24 +21,16 @@ def callback(bt_addr, rssi, packet, additional_info):
     if rssi > rangeMax:
         print("aqui, <%d>" %(rssi))
         output = commands.getoutput('ps -A')
-        #if 'chromium-browse' in output:
-        #    closeBrowser()
         print('Ejecucion Hilo Navegador')
         threading.Thread(target=openBrowserCam).start()
-        #os.system("sudo chromium-browser -no-sandbox http://www.google.com.co")
-        #print("NAVEGADOR ABIERTO -----------------------------")
-        #print('Delay de espera')
         time.sleep(15) #del
-        #print('Cerrando Navegador')
-	closeBrowser()
-	#print("Close interno")
-	#threading.Thread(target=closeBrowser).start()
-        #closeBrowser()
+        closeBrowser()
+
+
 # scan for all iBeacon advertisements from beacons with the specified uuid
 print("Iniciando Hilo de Scanner")
 scanner = BeaconScanner(callback,
     device_filter=IBeaconFilter(uuid="00000003-0000-0000-0000-000000000000"))
-#threading.current_thread()
 
 scanner.start()
 print(threading.current_thread())
